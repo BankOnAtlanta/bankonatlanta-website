4be53ba4822713d77e44c5bb27e25d8e707fa730
David Garrett <garrettdavid1@gmail.com>
2018-03-28 21:19:17 -0400

    Added John's new participate code.

--- scripts/components/participate.js
+++ scripts/components/participate.js
@@ -4,12 +4,9 @@ lib.doWhenExists("#participate_form", function() {
     .addEventListener("submit", function(evt) {
       evt.preventDefault(); // prevent the form from submitting (until we want it to)
 
+      // **********
+      // Stuff to configure:
       var email_address = "bankonatlanta@gmail.com";
-
-      var form_value_name = $('input[name="name"]').val(),
-        form_value_phone = $('input[name="phone"]').val(),
-        form_value_interests = $('select[name="interests"]').val(),
-        form_value_comments = $('textarea[name="comments"]').val();
 
       var email_body =
         "User {name} requests more information about Bank On Atlanta.\n\n" +
@@ -18,17 +15,18 @@ lib.doWhenExists("#participate_form", function() {
         "Comments: {comments}";
 
       var email_subject = "User request for more info about Bank On Atlanta";
-      var email_content = "";
-      var email_url = "";
+      // End of stuff to configure
+      // **********
 
-      if (
-        form_value_name &&
-        form_value_phone &&
-        form_value_interests &&
-        form_value_comments
-      ) {
-        // form is totally filled-out, carry on
+      var form_value_name = $('input[name="name"]').val(),
+        form_value_phone = $('input[name="phone"]').val(),
+        form_value_interests = $('select[name="interests"]').val(),
+        form_value_comments = $('textarea[name="comments"]').val();
 
+      var email_content, email_url;
+
+      // Required fields: name & interests only
+      if (form_value_name && form_value_interests) {
         email_content = email_body
           .replace("{name}", form_value_name)
           .replace("{phone}", form_value_phone)
@@ -38,15 +36,15 @@ lib.doWhenExists("#participate_form", function() {
         email_url = encodeURI(
           "mailto:" +
             email_address +
-            "?Subject=" +
+            "?subject=" +
             email_subject +
-            "&Body=" +
+            "&body=" +
             email_content
         );
 
         window.open(email_url, "_blank");
       } else {
-        // form isn't fully filled-out yet
+        // Required fields not filled out.
         alert("Please fill out the form completely before submitting.");
       }
     });
